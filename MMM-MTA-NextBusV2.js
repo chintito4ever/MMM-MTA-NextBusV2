af8d2c20bb4fe86e113d88559156279dde7a5158
chintito4ever <41648578+chintito4ever@users.noreply.github.com>
2023-05-10 02:27:51 -0400

    Update MMM-MTA-NextBusV2.js

--- MMM-MTA-NextBusV2.js
+++ MMM-MTA-NextBusV2.js
@@ -7,7 +7,7 @@
  * MIT Licensed.
  */
 
-Module.register("MMM-MTA-NextBusV2", {
+Module.register("MMM-MTA-NextBus", {
 	defaults: {
 		timeFormat: config.timeFormat,
 		maxEntries: 5,
@@ -31,56 +31,38 @@ Module.register("MMM-MTA-NextBusV2", {
 		setInterval(function() {
 			self.sendSocketNotification("GET_DATA");
 		}, this.config.updateInterval);
-
-	},
+		
+	},	
 
 	getDom: function() {
-	  var wrapper = document.createElement("div");
-	  wrapper.className = "nextbus";
+		var self = this;
 
-	  if (!this.dataRequest) {
-	    wrapper.innerHTML = "Loading...";
-	    return wrapper;
-	  }
+		// create element wrapper for show into the module
+		var wrapper = document.createElement("div");
+		// If this.dataRequest is not empty
+		if (this.dataRequest) {
+			this.dataRequest.forEach(function(data, i) {
+				var wrapperDataRequest = document.createElement("div");
+				
+				wrapperDataRequest.innerHTML = data;
+				wrapperDataRequest.className = "small";
+	
+				wrapper.appendChild(wrapperDataRequest);
+			});
 
-	  var table = document.createElement("table");
-	  table.className = "small";
-
-	  this.dataRequest.forEach(function(bus) {
-	    var tr = document.createElement("tr");
-
-	    var tdLine = document.createElement("td");
-	    tdLine.innerHTML = bus.PublishedLineName;
-	    tr.appendChild(tdLine);
-
-	    var tdDest = document.createElement("td");
-	    tdDest.innerHTML = bus.DestinationName;
-	    tr.appendChild(tdDest);
-
-	    var tdTime = document.createElement("td");
-	    tdTime.innerHTML = bus.ExpectedArrivalTime;
-	    tr.appendChild(tdTime);
-
-	    table.appendChild(tr);
-	  });
-
-	  wrapper.appendChild(table);
-
-	  var lastUpdate = document.createElement("div");
-	  lastUpdate.innerHTML = "Last Updated: " + this.formatTimeString(this.lastUpdate);
-	  lastUpdate.className = "xsmall last-update";
-	  wrapper.appendChild(lastUpdate);
-
-	  return wrapper;
+			
+		}
+		
+		return wrapper;
 	},
 
 	getScripts: function() {
 		return ["moment.js"];
 	},
 
-	getStyles: function() {
+	getStyles: function () {
 		return [
-			"MMM-MTA-NextBusV2.css",
+			"MMM-MTA-NextBus.css",
 		];
 	},
 
@@ -97,48 +79,57 @@ Module.register("MMM-MTA-NextBusV2", {
 	},
 
 	processActionNextBus: function(response) {
-	  var result = [];
-	  var serviceDelivery = response.Siri.ServiceDelivery;
+		
+		var result = [];
+		
+		var serviceDelivery = response.Siri.ServiceDelivery;
+		var updateTimestampReference = new Date(serviceDelivery.ResponseTimestamp);
+		
+		//console.log(updateTimestampReference);
+		
+		// array of buses
+		var visits = serviceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit;
+		var visitsCount = Math.min(visits.length, this.config.maxEntries);
+		
+		for (var i = 0; i < visitsCount; i++) {
+			r = '';
 
-	  // Get the current time from the response, as a reference for calculating time differences
-	  var responseTimestamp = new Date(serviceDelivery.ResponseTimestamp);
+			var journey = visits[i].MonitoredVehicleJourney;
+			var line = journey.PublishedLineName[0]; 
+			
+			var destinationName = journey.DestinationName[0];
+			if (destinationName.startsWith('LIMITED')) {
+				line += ' LIMITED';
+			}
+			
+			r += line + ', ';
+			
+			var monitoredCall = journey.MonitoredCall;
+			// var expectedArrivalTime = new Date(monitoredCall.ExpectedArrivalTime);
+			if (monitoredCall.ExpectedArrivalTime) {
+				var mins = this.getArrivalEstimateForDateString(monitoredCall.ExpectedArrivalTime, updateTimestampReference);
+				r += mins + ', ';
+			}
+			
+			
+			var distance = monitoredCall.ArrivalProximityText;
+			r += distance;
 
-	  // Iterate over the MonitoredStopVisit array
-	  var monitoredStopVisits = serviceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit;
-	  for (var i = 0; i < monitoredStopVisits.length && i < this.config.maxEntries; i++) {
-	    var monitoredVehicleJourney = monitoredStopVisits[i].MonitoredVehicleJourney;
-	    var publishedLineName = monitoredVehicleJourney.PublishedLineName[0];
-	    var destinationName = monitoredVehicleJourney.DestinationName[0];
-	    var arrivalTime = monitoredVehicleJourney.MonitoredCall.ExpectedArrivalTime;
+			result.push(r);
+		}
 
-	    // Check if the expected arrival time is provided in the response
-	    if (arrivalTime) {
-	      // Calculate the time difference between the expected arrival time and the response timestamp, in minutes
-	      var arrivalTimeDiff = Math.floor((new Date(arrivalTime) - responseTimestamp) / 60000);
 
-	      // Format the arrival time difference as a string, e.g. "2 minutes"
-	      var arrivalTimeStr = arrivalTimeDiff + " minute" + (arrivalTimeDiff === 1 ? "" : "s");
 
-	      // Build the result string for this stop
-	      var resultStr = publishedLineName + ", " + arrivalTimeStr + ", " + destinationName;
-	      result.push(resultStr);
-	    }
-	  }
-
-	  // Add the "last updated" string to the result array
-	  var lastUpdatedStr = "Last updated: " + this.formatTimeString(responseTimestamp);
-	  result.push(lastUpdatedStr);
-
-	  return result;
+		result.push('Last Updated: ' + this.formatTimeString(updateTimestampReference));
+		
+		return result;
 	},
-
-
 
 	getArrivalEstimateForDateString: function(dateString, refDate) {
 		var d = new Date(dateString);
-
+		
 		var mins = Math.floor((d - refDate) / 60 / 1000);
-
+		
 		return mins + ' minute' + ((Math.abs(mins) === 1) ? '' : 's');
 	},
 
@@ -159,13 +150,11 @@ Module.register("MMM-MTA-NextBusV2", {
 	},
 
 	// socketNotificationReceived from helper
-	socketNotificationReceived: function(notification, payload) {
-	    if (notification === "DATA") {
-		console.log(payload); // Add this line
-		this.processData(payload);
-	    } else if (notification === "ERROR") {
-		self.updateDom(self.config.animationSpeed);
-	    }
+	socketNotificationReceived: function (notification, payload) {
+		if (notification === "DATA") {
+			this.processData(payload);
+		} else if (notification === "ERROR") {
+			self.updateDom(self.config.animationSpeed);
+		} 
 	},
-
 });
