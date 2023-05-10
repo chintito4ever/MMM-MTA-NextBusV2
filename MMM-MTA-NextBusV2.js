4feffd716a55ce9f8bf7d86e0a2b40d764c657c7
chintito4ever <41648578+chintito4ever@users.noreply.github.com>
2023-05-10 01:02:32 -0400

    Update MMM-MTA-NextBusV2.js

--- MMM-MTA-NextBusV2.js
+++ MMM-MTA-NextBusV2.js
@@ -35,44 +35,29 @@ Module.register("MMM-MTA-NextBusV2", {
 	},
 
 	getDom: function() {
-	    var wrapper = document.createElement("div");
-	    wrapper.className = "nextbus";
+	  var self = this;
 
-	    if (!this.dataRequest) {
-		wrapper.innerHTML = "Loading...";
-		return wrapper;
-	    }
+	  // create element wrapper for show into the module
+	  var wrapper = document.createElement("div");
+	  // If this.dataRequest is not empty
+	  if (this.dataRequest) {
+	    this.dataRequest.forEach(function(data, i) {
+	      var wrapperDataRequest = document.createElement("div");
 
-	    var table = document.createElement("table");
-	    table.className = "small";
+	      var line = data.MonitoredVehicleJourney.PublishedLineName;
+	      var destination = data.MonitoredVehicleJourney.DestinationName;
+	      var arrivalTime = data.MonitoredVehicleJourney.MonitoredCall.ArrivalProximityText;
 
-	    this.dataRequest.forEach(function(bus) {
-		var tr = document.createElement("tr");
+	      wrapperDataRequest.innerHTML = 'Bus ' + line + ' to ' + destination + ' arriving ' + arrivalTime;
+	      wrapperDataRequest.className = "small";
 
-		var tdLine = document.createElement("td");
-		tdLine.innerHTML = bus.lineRef;
-		tr.appendChild(tdLine);
-
-		var tdDirection = document.createElement("td");
-		tdDirection.innerHTML = bus.directionRef;
-		tr.appendChild(tdDirection);
-
-		var tdDistance = document.createElement("td");
-		tdDistance.innerHTML = bus.distanceFromStop;
-		tr.appendChild(tdDistance);
-
-		table.appendChild(tr);
+	      wrapper.appendChild(wrapperDataRequest);
 	    });
+	  }
 
-	    wrapper.appendChild(table);
-
-	    var lastUpdate = document.createElement("div");
-	    lastUpdate.innerHTML = "Last Updated: " + this.formatTimeString(this.lastUpdate);
-	    lastUpdate.className = "xsmall last-update";
-	    wrapper.appendChild(lastUpdate);
-
-	    return wrapper;
+	  return wrapper;
 	},
+
 
 
 	getScripts: function() {
@@ -98,30 +83,18 @@ Module.register("MMM-MTA-NextBusV2", {
 	},
 
 	processActionNextBus: function(response) {
-	    var result = [];
-
-	    var serviceDelivery = response.Siri.ServiceDelivery;
-	    var updateTimestampReference = new Date(serviceDelivery.ResponseTimestamp);
-
-	    // array of buses
-	    var monitoredStopVisit = serviceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit;
-
-	    for (var i = 0; i < monitoredStopVisit.length && i < this.config.maxEntries; i++) {
-		var journey = monitoredStopVisit[i].MonitoredVehicleJourney;
-
-		var line = journey.PublishedLineName[0];
-		var destinationName = journey.DestinationName[0];
-
-		var monitoredCall = journey.MonitoredCall;
-		var distance = monitoredCall.ArrivalProximityText;
-
-		var r = line + " to " + destinationName + ", " + distance;
-		result.push(r);
-	    }
-
-	    result.push('Last Updated: ' + this.formatTimeString(updateTimestampReference));
-
-	    return result;
+	  var self = this;
+	  if (response.data.Siri.ServiceDelivery.VehicleMonitoringDelivery) {
+	    var buses = response.data.Siri.ServiceDelivery.VehicleMonitoringDelivery[0].VehicleActivity;
+	    var dataRequest = [];
+	    buses.forEach(function(bus) {
+	      dataRequest.push(bus);
+	    });
+	    self.dataRequest = dataRequest;
+	    self.updateDom();
+	  } else {
+	    Log.info('Next Bus Error. No data.');
+	  }
 	},
 
 
